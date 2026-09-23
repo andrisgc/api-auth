dc764102493d13da9b28dfdc8b33aac7f40a7f69
andrisgc <andrisgcastro4@gmail.com>
2026-09-23 13:53:37 -0300

    feat: implementa entidade User com UserDetails e UserRepository

---
+++ src/main/java/com/agc/auth/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.agc.auth.repository;
+
+import com.agc.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByEmail(String email);
+}
